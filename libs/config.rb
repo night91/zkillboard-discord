27c4d9313869d54cac589fc6c3acf3921de6b022
Eddy Roberto <eddyr.morales@gmail.com>
2018-02-22 12:40:35 -0600

    Update config files to read

--- libs/config.rb
+++ libs/config.rb
@@ -1,7 +1,7 @@
 module ZkbDiscord
   class Config
     CONFIG_FILES = %w[config application]
-    APPLICATIONS_FILES= %w[corp goons]
+    APPLICATIONS_FILES= %w[corp goons pandemic-horde pandemic-legion test]
 
     attr_reader :config
 
